93527b4cde60ac972263c6bbbf177d19d1961868
Andy Park <andy@bigbearlabs.com>
2023-05-18 11:24:35 +0900

    checkpoint layout after 1st build

--- keyboards/ergomax/keymaps/default/keymap.c
+++ keyboards/ergomax/keymaps/default/keymap.c
@@ -28,30 +28,30 @@ enum custom_keycodes {
   QMKURL
 };
 
-#define LUP_TG TG(_UP)
+#define LUP_MO MO(_UP)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT( /* Base */
-               KC_ESC,  KC_F2,   KC_F3,   KC_F4, KC_F5, KC_F6,                         KC_F7,       KC_F8, KC_F9,   KC_F10,   KC_F11,  KC_F12,                  \
-    KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,  KC_NO,                         KC_NO,       KC_6,  KC_7,    KC_8,     KC_9,    KC_0,      KC_MINUS,     \
-    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,  KC_LBRACKET,                   KC_RBRACKET, KC_Y,  KC_U,    KC_I,     KC_O,    KC_P,      KC_EQUAL,     \
-    KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,                                              KC_H,  KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,     \
-    KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,  LUP_TG,                        KC_BSLASH,   KC_N,  KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,    \
-    KC_NO,     KC_LGUI, KC_LALT, KC_NO,   KC_LCTRL,                                                        KC_DOWN, KC_UP,    KC_LEFT, KC_RIGHT,  KC_INS,       \
-                                                        KC_NO,    KC_NO,      KC_NO,   KC_NO,                                                                   \
-                                              KC_BSPC,  KC_DEL,   KC_PGUP,    KC_HOME, KC_ENTER,  KC_SPACE,                                                     \
-                                                                  KC_PGDOWN,  KC_END                                                                            \
+               KC_F1,  KC_F2,   KC_F3,   KC_F4, KC_F5, KC_F6,                         KC_F7,       KC_F8, KC_F9,   KC_F10,   KC_F11,  KC_F12,                  \
+    KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,  KC_EQUAL,                      KC_RBRACKET, KC_6,  KC_7,    KC_8,     KC_9,    KC_0,      KC_BSPC,     \
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,  KC_MINUS,                      KC_LBRACKET, KC_Y,  KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,     \
+    KC_CAPSLOCK, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,                                              KC_H,  KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_ENTER,     \
+    KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,  LUP_MO,                        KC_QUOTE,   KC_N,  KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,    \
+    KC_LCTRL,  KC_LGUI, KC_LALT, KC_NO,   KC_LGUI,                                                        KC_RGUI, KC_NO,    KC_RALT, KC_F13,  KC_RCTRL,       \
+                                                        KC_NO,    KC_NO,      KC_NO,   KC_ESC,                                                                   \
+                                              KC_SPACE,  LUP_MO,   KC_PGUP,    KC_HOME, KC_ENTER,  KC_SPACE,                                                     \
+                                                                  KC_PGDN,  KC_END                                                                            \
   ),
   [_UP] = LAYOUT(
                KC_F1,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,                     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,              \
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    \
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    \
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    \
-    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LUP_TG,                    KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    \
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,     KC_TRNS, KC_TRNS, KC_UP,  KC_TRNS,  KC_TRNS,  KC_TRNS,    \
+    KC_TRNS,   KC_LSHIFT, KC_LCTRL, KC_LALT, KC_LGUI, KC_TRNS,                                         KC_TRNS, KC_LEFT, KC_DOWN,  KC_RIGHT,  KC_TRNS,  KC_TRNS,    \
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LUP_MO,                    KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    \
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                           KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    \
-                                                       KC_TRNS,   KC_TRNS,    KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,                                         \
-                                              KC_TRNS, KC_TRNS,   KC_TRNS,    KC_AUDIO_VOL_UP,      KC_MPLY, KC_TRNS,                                           \
-                                                                  KC_TRNS,    KC_AUDIO_VOL_DOWN                                                                 \
+                                                       KC_TRNS,   KC_TRNS,    KC_X, KC_MEDIA_NEXT_TRACK,                                         \
+                                              KC_TRNS, KC_TRNS,   KC_TRNS,    KC_C,      KC_MPLY, KC_TRNS,                                           \
+                                                                  KC_TRNS,    KC_V                                                                 \
   )
 };
 
