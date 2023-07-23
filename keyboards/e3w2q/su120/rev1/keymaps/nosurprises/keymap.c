bd9183d261af7946b740f579af93e738ce1c0a34
Andy Park <andy@bigbearlabs.com>
2023-07-23 20:52:11 +0900

    fix cmd-c macro key

--- keyboards/e3w2q/su120/rev1/keymaps/nosurprises/keymap.c
+++ keyboards/e3w2q/su120/rev1/keymaps/nosurprises/keymap.c
@@ -66,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT,        KC_1,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_6,     KC_7,     KC_8,
     KC_LCTL,        LT(1,KC_F13), KC_LALT,  KC_LCMD,  KC_SPC,     KC_5,     KC_6,     KC_7,     KC_8,     XXXXXXX,
 
-    KC_F10,      KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,
-    LGUI(KC_V),  KC_0,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,     KC_BSPC,     KC_7,   
+    LGUI(KC_C),    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,
+    LGUI(KC_V),  KC_F10,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,     KC_BSPC,     KC_7,   
     LGUI(S(KC_G)),KC_Y,  KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,     KC_BSLS,     KC_8,  
     LGUI(KC_G),   KC_H,  KC_J,     KC_K,     KC_L,     KC_SCLN,     KC_QUOT,     KC_6,     KC_ENT,     KC_8,    
     KC_0,    LGUI(KC_Z),  KC_N,     KC_M,     KC_COMM,     KC_DOT,     KC_SLSH,     KC_6,     KC_RSFT,     KC_8,  
