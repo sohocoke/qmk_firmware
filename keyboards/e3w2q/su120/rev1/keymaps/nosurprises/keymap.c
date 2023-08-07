e1f25028bfc766fb0c3d1e0e4adde9b6705166d8
Andy Park <andy@bigbearlabs.com>
2023-08-07 15:07:07 +0900

    more mapping tweaks

--- keyboards/e3w2q/su120/rev1/keymaps/nosurprises/keymap.c
+++ keyboards/e3w2q/su120/rev1/keymaps/nosurprises/keymap.c
@@ -59,28 +59,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [0] = LAYOUT( /* Base */ 
-    KC_0,           KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,
+    KC_ESC,         KC_F1,     KC_F2,   KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_7,     KC_8,     KC_9,
     KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     XXXXXXX,
     KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_6,     KC_7,     KC_8,     XXXXXXX,
     LT(1,KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_6,     KC_7,     KC_8,     XXXXXXX,
     KC_LSFT,        KC_1,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_6,     KC_7,     KC_8,
-    KC_LCTL,        LT(1,KC_F13), KC_LALT,  KC_LCMD,  KC_SPC,     KC_5,     KC_6,     KC_7,     KC_8,     XXXXXXX,
+    KC_LCTL,        LT(1,KC_F13), KC_LALT,  KC_LCMD,  MT(MOD_LGUI,KC_SPC),     KC_5,     KC_6,     KC_7,     KC_8,     XXXXXXX,
 
-    LGUI(KC_C),    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,
-    LGUI(KC_V),  KC_F10,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,     KC_BSPC,     KC_7,   
-    LGUI(S(KC_G)),KC_Y,  KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,     KC_BSLS,     KC_8,  
+    LGUI(KC_C),   XXXXXXX, KC_F7,   KC_F8,    KC_F9,    KC_F10,     KC_F11,     KC_F12,    KC_F14,     KC_9,
+    LGUI(KC_V),   LGUI(KC_D),  KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,     KC_BSPC,     KC_7,   
+    LGUI(KC_E),   KC_Y,  KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,     KC_BSLS,     KC_8,  
     LGUI(KC_G),   KC_H,  KC_J,     KC_K,     KC_L,     KC_SCLN,     KC_QUOT,     KC_6,     KC_ENT,     KC_8,    
     KC_0,    LGUI(KC_Z),  KC_N,     KC_M,     KC_COMM,     KC_DOT,     KC_SLSH,     KC_6,     KC_RSFT,     KC_8,  
     LGUI(KC_X),  KC_0,  KC_1,     KC_SPC,     KC_RCMD,     KC_RALT,     LUP_MO,     KC_ESC,     KC_RCTL,     KC_8
-  ),
+  ), 
 
   [1] = LAYOUT( /* Upper */
     KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     XXXXXXX,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_6,     KC_7,     KC_8,     XXXXXXX,
-    KC_TRNS,  C(KC_A), KC_LSHIFT, KC_LCTRL, KC_LALT, KC_LGUI,   KC_6,     KC_7,     KC_8,     XXXXXXX,
+    KC_TRNS,  KC_LSHIFT,KC_LCTRL, KC_LALT,  KC_F,      KC_G,     KC_6,     KC_7,     KC_8,     XXXXXXX,
     KC_LSFT,  KC_1,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_6,     KC_7,     KC_8,
-    KC_LCTL,  KC_F13,   KC_LALT,  KC_LCMD,  KC_SPC,     KC_5,     KC_6,     KC_7,     KC_8,     XXXXXXX,
+    KC_LCTL,  KC_F13,   KC_LALT,  KC_LCMD,  KC_LCMD,     KC_5,     KC_6,     KC_7,     KC_8,     XXXXXXX,
 
     KC_1,  KC_0,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    
     KC_2,  KC_0,  S(KC_7),     S(KC_8),     S(KC_9),     S(KC_0),     S(KC_MINS),     S(KC_EQL),     S(KC_BSPC),     KC_7,   
